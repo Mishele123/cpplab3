a041d235db2ffe2091bcfec98638a47c04b4a115
Mishele123 <misha228665161@gmail.com>
2023-11-28 12:37:56 +0400

    Функции шаблоны | Собственный класс

--- include/header.h
+++ include/header.h
@@ -11,17 +11,153 @@ struct stats
 	size_t copy_count = 0;
 };
 
-stats SelectionSort(std::vector<int>&v);
+
+class CustomClass
+{
+public:
+	int _value;
+	CustomClass(int value) : _value(value) {}
+
+	bool operator<(const CustomClass& other) const
+	{
+		return _value < other._value;
+	}
+	bool operator>(const CustomClass& other) const
+	{
+		return !(_value < other._value);
+	}
+};
+
+template <typename T>
+stats SelectionSort(std::vector<T>& v)
+{
+
+	stats a;
+
+	size_t n = v.size();
+
+	for (size_t i = 0; i < n - 1; i++)
+	{
+		size_t minIndex = i;
+		for (size_t j = i + 1; j < n; j++)
+		{
+			a.comparison_count++;
+			if (v[minIndex] > v[j])
+				minIndex = j;
+		}
+		if (i != minIndex)
+		{
+			std::swap(v[minIndex], v[i]);
+			a.copy_count += 2;
+		}
+	}
+	return a;
+}
 
 
+// Быстрая сортировка
+template <typename T>
+size_t partition(std::vector<T>& arr, size_t low, size_t high, stats& s)
+{
+	T pivot = arr[high];
+	size_t i = low;
 
-size_t partition(std::vector<int>& arr, size_t low, size_t high, stats& s);
+	for (size_t j = low; j < high; j++)
+	{
+		s.comparison_count++;
+		if (arr[j] < pivot)
+		{
+			std::swap(arr[i], arr[j]);
+			s.copy_count += 2;
+			i++;
+		}
+	}
 
-void QuickSort(std::vector<int>& arr, size_t low, size_t high, stats& s);
+	std::swap(arr[i], arr[high]);
+	s.copy_count += 2;
 
-stats QuickSort(std::vector<int>& arr);
+	return i;
+}
 
-void merge(std::vector<int>& arr, size_t left, size_t middle, size_t right, stats& s);
-void MergeSort(std::vector<int>& arr, size_t left, size_t right, stats& s);
+template <typename T>
+void QuickSort(std::vector<T>& arr, size_t low, size_t high, stats& s)
+{
+	if (low < high) {
+		size_t pivotIndex = partition(arr, low, high, s);
+		if (pivotIndex > 0)
+		{
+			QuickSort(arr, low, pivotIndex - 1, s);
+		}
 
-stats NaturalMergeSort(std::vector<int>& arr);
+		QuickSort(arr, pivotIndex + 1, high, s);
+	}
+}
+
+template <typename T>
+stats QuickSort(std::vector<T>& arr)
+{
+	stats s;
+	size_t n = arr.size();
+	QuickSort(arr, 0, n - 1, s);
+	return s;
+}
+
+
+template <typename T>
+stats NaturalMergeSort(std::vector<T>& arr)
+{
+	stats s;
+	MergeSort(arr, 0, arr.size() - 1, s);
+	return s;
+}
+
+template <typename T>
+void MergeSort(std::vector<T>& arr, size_t left, size_t right, stats& s)
+{
+	if (left < right)
+	{
+		size_t mid = left + (right - left) / 2;
+		MergeSort(arr, left, mid, s);
+		MergeSort(arr, mid + 1, right, s);
+		merge(arr, left, mid, right, s);
+	}
+}
+
+template <typename T>
+void merge(std::vector<T>& arr, size_t left, size_t middle, size_t right, stats& s)
+{
+	size_t i = left;
+	size_t j = middle + 1;
+	std::vector<T> temp;
+	while (i <= middle && j <= right)
+	{
+		s.comparison_count++;
+		if (arr[i] < arr[j])
+		{
+			temp.push_back(arr[i++]);
+			s.copy_count++;
+		}
+		else
+		{
+			temp.push_back(arr[j++]);
+			s.copy_count++;
+		}
+	}
+
+	while (i <= middle)
+	{
+		temp.push_back(arr[i++]);
+		s.copy_count++;
+	}
+	while (j <= right)
+	{
+		temp.push_back(arr[j++]);
+		s.copy_count++;
+	}
+
+	for (size_t i = left; i <= right; i++)
+	{
+		arr[i] = temp[i - left];
+		s.copy_count++;
+	}
+}
--- src/main.cc
+++ src/main.cc
@@ -2,49 +2,196 @@
 
 #include "header.h"
 #include <random>
+#include <string>
 
 // 1 2 2 3 4 4
 
-
-
-
-int main() {
-    std::vector<int> arr = { 9, 5, 2, 2, 7, 1, 8, 3, 3 };
-
-
-    /*std::random_device device;
+template <typename T>
+std::vector<T> generateRandomVector(size_t size)
+{
+    std::vector<T> arr(size);
+    std::random_device device;
     std::mt19937 generator(device());
-    std::uniform_int_distribution<int> distr(0, INT_MAX);
-
-    for (int n = 0; n < 10000; ++n) 
-    {
-        arr.push_back(distr(generator));
-    }*/
-
-    for (auto& i : arr)
-    {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
-
-    stats s;
-
-    s = NaturalMergeSort(arr);
-
-    // s = SelectionSort(arr);
-
-    std::cout << "Sorted array: ";
+    std::uniform_int_distribution<T> distr(0, 999);
 
     for (auto &i : arr)
     {
+        i = distr(generator);
+    }
+
+    return arr;
+}
+
+template <typename T>
+std::vector<T> generateSortedVector(size_t size)
+{
+    std::vector<T> arr(size);
+    for (size_t i = 0; i < size; i++)
+    {
+        arr.push_back(static_cast<T>(i));
+    }
+    return arr;
+}
+
+template <typename T>
+std::vector<T> generateReverseSortedVector(size_t size)
+{
+    std::vector<T> arr;
+    for (size_t i = size - 1; int(i) >= 0; --i)
+    {
+        arr.push_back(static_cast<T>(i));
+    }
+    return arr;
+}
+
+int main() 
+{
+    std::vector<size_t> vector_lenghts = { 1000, 2000, 3000, 4000, 5000, 6000,
+        7000, 8000, 9000, 10000, 25000, 50000 };
+
+    //size_t count = 100;
+
+    //for (size_t &size : vector_lenghts)
+    //{   
+    //    size_t comparison_count_average = 0;
+    //    size_t copy_count_average = 0;
+
+    //    for (size_t i = 0; i < count; i++)
+    //    {
+    //        std::vector<int> randomVector = generateRandomVector<int>(size);
+
+    //        stats s;
+
+    //        s = NaturalMergeSort(randomVector);
+
+
+    //        /*std::cout << "Lenght = " << size << std::endl;
+    //        std::cout << "Size arr = " << randomArray.size() << std::endl;
+    //        std::cout << "Number of comparisons: " << s.comparison_count << std::endl;
+    //        std::cout << "Number of copies: " << s.copy_count << std::endl;*/
+
+    //        comparison_count_average += s.comparison_count;
+    //        copy_count_average += s.copy_count;
+    //    }
+
+    //    comparison_count_average /= count;
+    //    copy_count_average /= count;
+
+    //    
+    //    std::cout << "For vector size = " << size << std::endl;
+    //    std::cout << "comparison_count_average = " << comparison_count_average << std::endl;
+    //    std::cout << "copy_count_average = " << copy_count_average << std::endl;
+    //    
+    //    // Для отсортированного
+    //    /*stats s1;
+    //    std::vector<int> arr1 = generateSortedVector(size);
+    //    s1 = NaturalMergeSort(arr1);
+    //    std::cout << "generateSortedArray = " << size << std::endl;
+    //    std::cout << "comparison_count_average = " << s1.comparison_count << std::endl;
+    //    std::cout << "copy_count_average = " << s1.copy_count << std::endl;*/
+
+    //    // Для реверснутого
+    //    /*stats s2;
+    //    std::vector<int> arr2 = generateReverseSortedVector(size);
+    //    s2 = NaturalMergeSort(arr2);
+    //    std::cout << "generateSortedArray = " << size << std::endl;
+    //    std::cout << "comparison_count_average = " << s2.comparison_count << std::endl;
+    //    std::cout << "copy_count_average = " << s2.copy_count << std::endl;*/
+    //}
+
+    /*std::vector<std::string> vec = { "banana", "apple", "abple", "orange", "grape", "kiwi"};
+
+    for (auto& i : vec)
+    {
         std::cout << i << " ";
     }
     std::cout << std::endl;
+    
+    SelectionSort(vec);
 
-    std::cout << "Size arr = " << arr.size() << std::endl;
-    std::cout << "Number of comparisons: " << s.comparison_count << std::endl;
-    std::cout << "Number of copies: " << s.copy_count << std::endl;
+    for (auto& i : vec)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;*/
+
+    std::vector<CustomClass> vec = { CustomClass(5), CustomClass(4), CustomClass(2), CustomClass(1), CustomClass(0) };
+    for (auto& i : vec)
+    {
+        std::cout << i._value << " ";
+    }
+    std::cout << std::endl;
+
+    SelectionSort(vec);
+
+    for (auto& i : vec)
+    {
+        std::cout << i._value << " ";
+    }
 
 
     return 0;
 }
+
+
+
+
+//For vector size = 1000
+//comparison_count_average = 499500
+//copy_count_average = 1983
+//For vector size = 2000
+//comparison_count_average = 1999000
+//copy_count_average = 3981
+//For vector size = 3000
+//comparison_count_average = 4498500
+//copy_count_average = 5979
+//For vector size = 4000
+//comparison_count_average = 7998000
+//copy_count_average = 7976
+//For vector size = 5000
+//comparison_count_average = 12497500
+//copy_count_average = 9974
+//For vector size = 6000
+//comparison_count_average = 17997000
+//copy_count_average = 11972
+//For vector size = 7000
+//comparison_count_average = 24496500
+//copy_count_average = 13972
+//For vector size = 8000
+//comparison_count_average = 31996000
+//copy_count_average = 15970
+//For vector size = 9000
+//comparison_count_average = 40495500
+//copy_count_average = 17968
+//For vector size = 10000
+//comparison_count_average = 49995000
+//copy_count_average = 19965
+//For vector size = 25000
+//comparison_count_average = 312487500
+//copy_count_average = 49934
+
+
+
+    //for (auto& i : arr)
+    //{
+    //    std::cout << i << " ";
+    //}
+    //std::cout << std::endl;
+
+    //stats s;
+
+    //s = NaturalMergeSort(arr);
+
+    //// s = SelectionSort(arr);
+
+    //std::cout << "Sorted array: ";
+
+    //for (auto &i : arr)
+    //{
+    //    std::cout << i << " ";
+    //}
+    //std::cout << std::endl;
+
+    /*std::cout << "Size arr = " << arr.size() << std::endl;
+    std::cout << "Number of comparisons: " << s.comparison_count << std::endl;
+    std::cout << "Number of copies: " << s.copy_count << std::endl;*/
